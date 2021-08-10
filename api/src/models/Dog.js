4c08d47e87fe171159bd78e1aa82fb6f8e08493d
LucioGomez <luciotony25@gmail.com>
2021-08-10 20:06:44 -0300

    v1

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -1,12 +1,40 @@
 const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
+
+
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
+    IDBDatabase: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull:false, 
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    height:{
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    weight:{
+      type: DataTypes.TEXT,
+      allowNull:false
+    },
+    life_span:{
+      type: DataTypes.STRING
+    },
+    url_image:{
+      type: DataTypes.STRING,
+
+    },
+    createInDb:{
+      type : DataTypes.BOOLEAN,
+      defaultValue:false,
+      allowNull:false
+    }
   });
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,59 @@
-const { Router } = require('express');
+const {
+    Router
+} = require('express');
+const axios = require('axios')
+require('sequelize');
+//Me traigo las tablas de la base de datos
+const {Dog,Temperament,dogs_temperaments} = require('../db.js');
+const { Sequelize } = require('sequelize');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+//TraerData
+const loadData = async () => {
+    const arrdata = await axios.get('https://api.thedogapi.com/v1/breeds?api_key=bd856d2c-0fea-4f95-92e2-dc351be490dc')
+    //console.log("Esto es arrdata", arrdata)
+    // let {name,id} = arrdata.find
+    // console.log(arrdata.id)
+    const apiInfo = await arrdata.data.map(el =>{
+        return{
+            name: el.name,
+            id: el.id,
+            height: el.height.metric,
+            weight: el.weight.metric,
+            img_url: el.image.url
+        } 
+    })
+    console.log(apiInfo);
+    return apiInfo;
+      
+}
+loadData()
+router.get("/dogs", async function (req, res, next) {
+    const dogsAll = await loadData();
+    try {
+        //traigo los datos de mi db
+        let doglenght = await Dog.findAll()
+            // si no hay datos los cargo 
+    if(!doglenght.lenght){ 
+        await Dog.bulkCreate(dogsAll) 
+         console.log(dogsAll)
+         res.json("entre al if")
+    }
+    //res.json("no entre")
+    }        
+     catch (error) {
+        next(error)
+    }
+})
 
+/*
+Dog hasmany Temperaments
+Temperaments hasmany Dog
+*/
 
 module.exports = router;
